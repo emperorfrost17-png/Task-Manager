27082f132fb1901627a4427c1875aa2c23817e12
emperorfrost17-png <emperor.frost17@gmail.com>
2026-06-04 03:14:26 +0000

    used dataset to get index

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
 import { tasks } from "./task.js";
 
 function renderPage() {
-function renderTasks() {
-  let taskHTML = "";
-  tasks.forEach((task, index) => {
-    taskHTML += ` <div class="task-item" data-index="${index}">
+  function renderTasks() {
+    let taskHTML = "";
+    tasks.forEach((task, index) => {
+      taskHTML += ` <div class="task-item" data-index="${index}">
               <button class="task-check" aria-label="Mark task as completed">
                 <i class="fa-regular fa-circle"></i>
               </button>
@@ -25,16 +25,23 @@ function renderTasks() {
                 <button aria-label="Edit task" data-name="${task.title}">
                   <i class="fa-regular fa-pen-to-square"></i>
                 </button>
-                <button aria-label="Delete task">
+                <button aria-label="Delete task" class="js-delete-button">
                   <i class="fa-regular fa-trash-can"></i>
                 </button>
               </div>
             </div>`;
-  });
-  document.querySelector(".js-task-list").innerHTML = taskHTML;
-}
+    });
+    document.querySelector(".js-task-list").innerHTML = taskHTML;
+  }
+  renderTasks();
 
-renderTasks();
+  document.querySelectorAll(".task-item").forEach((item) => {
+    item.addEventListener("click", () => {
+      const index = item.dataset.index;
+      //This will give us the task object that was clicked on, we can use this to populate the edit form or perform other actions
+      const task = tasks[Number(index)];
+      console.log(`Clicked task: ${task.title}`);
+    });
+  });
 }
 renderPage();
-
